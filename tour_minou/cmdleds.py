035a5e779517331c9ff410d2cc2c3154e0256362
bernard <bernard@pinochet.fr>
2021-01-31 15:54:00 +0100

    Creation du projet contenant l'eclairage de la tour du minou

---
+++ tour_minou/cmdleds.py
@@ -0,0 +1,228 @@
+import requests;
+from time import sleep
+from random import randint
+
+lstCmds=[
+    [(0,255,0,0,0)],
+    [(0,12,127,0,0),(13,25,0,127,0),(26,34,0,0,127)],
+    ]
+
+class BdLeds:
+    ip=None
+    nb_leds=0
+    leds=[];
+
+    def apply(self):
+        dta="AA%02x" % (self.nb_leds);
+        for (r,g,b) in self.leds:
+            dta+="%02x%02x%02x" % (r,g,b)
+            
+        print(dta);
+        r = requests.post('http://%s/leds/write' % (self.ip),data=dta)
+        print(r.text);
+
+    def get_info(self):
+        r = requests.get('http://%s/leds/info' % (self.ip))
+        return r.text;
+        
+
+    def setAllLeds(self,col):
+        for i in range(self.nb_leds):
+            self.leds[i]=col;
+
+    def clearAllLeds(self):
+        self.setAllLeds((0,0,0))
+    
+    def __init__(self,nb_leds,ip):
+        if (nb_leds>255):
+            raise Exception("Trop grand!!")
+
+        self.ip=ip;
+        self.nb_leds=nb_leds
+        self.leds=[(0,0,0) for i in range(nb_leds)]
+
+        
+
+class GrpLd:
+    lds=None
+    list_leds=[]
+    def __init__(self,lds):
+        self.lds=lds
+        self.list_leds=[]
+    def addLed(self,n):
+        if n<self.lds.nb_leds:
+            self.list_leds.append(n)
+    def addLeds(self,nStart, nEnd):
+        for i in range(nStart,nEnd):
+            if i<self.lds.nb_leds:
+                self.list_leds.append(i)
+    def setColor(self,col):
+        for i in self.list_leds:
+            lds.leds[i]=col
+    def clear(self):
+        for i in self.list_leds:
+            lds.leds[i]=(0,0,0)
+        
+NUM_LEDS=34
+
+lds=BdLeds(NUM_LEDS,"192.168.1.42");
+print(lds.get_info());
+
+lds.setAllLeds((0,0,0));
+lds.apply()
+
+grpAr=GrpLd(lds)
+grpAr.addLeds(0, 13)
+grpAv=GrpLd(lds)
+grpAv.addLeds(13, 26)
+grpInt=GrpLd(lds)
+grpInt.addLeds(26, 34)
+
+grpAr1=GrpLd(lds)
+grpAr1.addLeds(9,12)
+grpAr2=GrpLd(lds)
+grpAr2.addLeds(4, 6)
+grpAr3=GrpLd(lds)
+grpAr3.addLeds(0, 2)
+
+grpAv1=GrpLd(lds)
+grpAv1.addLeds(14,17)
+grpAv2=GrpLd(lds)
+grpAv2.addLeds(19, 22)
+grpAv3=GrpLd(lds)
+grpAv3.addLeds(23,26)
+
+grpTour=GrpLd(lds)
+grpTour.addLeds(0,26)
+
+
+
+
+def bbr_alt():
+    grpInt.setColor((0,0,127))
+    lds.apply();
+
+    while True:
+        grpAv1.setColor((0,0,127))
+        grpAv2.setColor((127,127,127))
+        grpAv3.setColor((127,0,0))
+        grpAr1.clear();
+        grpAr2.clear();
+        grpAr3.clear();
+        lds.apply();
+
+        sleep(1)
+
+        grpAr1.setColor((0,0,127))
+        grpAr2.setColor((127,127,127))
+        grpAr3.setColor((127,0,0))
+        grpAv1.clear();
+        grpAv2.clear();
+        grpAv3.clear();
+        lds.apply();
+
+        sleep(1)
+
+def dep_3ld():
+    grpInt.setColor((0,127,0))
+    lds.apply();
+    
+    while True:
+        col=(127,127,0)
+        
+        grpTour.clear();
+        grpAv1.setColor(col)
+        lds.apply();
+
+        grpTour.clear();
+        grpAv2.setColor(col)
+        lds.apply();
+
+        grpTour.clear();
+        grpAv3.setColor(col)
+        lds.apply();
+
+def dep_3ld2():
+    grpInt.setColor((0,127,0))
+    lds.apply();
+    
+    while True:
+        cols=[
+            (127,0,0),
+            (0,127,0),
+            (0,0,127),
+            (127,127,0),
+            (0,127,127),
+            (127,0,127),
+            (127,127,127)
+            ];
+
+        for col in cols:
+            for i in range(13-3):
+                grpTour.clear();
+                lds.leds[i]=col
+                lds.leds[i+1]=col
+                lds.leds[i+2]=col
+                lds.leds[i+13]=col
+                lds.leds[i+13+1]=col
+                lds.leds[i+13+2]=col
+                lds.apply();
+
+        for col in cols:
+            (r,g,b)=col;
+            if (r==127):
+                r=1;
+            if (g==127):
+                g=1;
+            if (b==127):
+                b=1;
+
+            for i in range (0,80,5):
+                if (r!=0):
+                    r+=5;
+                if (g!=0):
+                    g+=5;
+                if (b!=0):
+                    b+=5;
+
+                if (r>127):
+                    r=127;
+                if (g>127):
+                    g=127;
+                if (b>127):
+                    b=127;
+
+                grpTour.setColor((r,g,b))
+                lds.apply();
+                if r==127 and g==127 and b==127:
+                    break;
+
+                sleep(0.05)
+
+            for i in range (0,80,5):
+                r-=5;
+                g-=5;
+                b-=5;
+                if (r<0):
+                    r=0;
+                if (g<0):
+                    g=0;
+                if (b<0):
+                    b=0;
+
+                grpTour.setColor((r,g,b))
+                lds.apply();
+                if r==0 and g==0 and b==0:
+                    break;
+                
+                
+            
+
+#bbr_alt();
+#dep_3ld2()
+
+#grpTour.setColor((127,0,0))
+#grpInt.setColor((0,127,0))
+lds.clearAllLeds()
+lds.apply();
+
---
+++ tour_minou/tour_minou.ino
@@ -0,0 +1,583 @@
+/**
+ * @file tour_minou.ino
+ * 
+ * @brief Gestion de l'éclairage de la tour de Minou
+*/
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+
+#include <Wire.h>
+#include "FastLED.h"
+#include "ldanim.h"
+
+#include "wifi_code.h"
+
+
+
+const char* ssid = STASSID;
+const char* password = STAPSK;
+
+ESP8266WebServer server(80);
+
+/**
+ * @brief Configuration du bandeau de LEDs
+ * @{
+*/
+#define POWER_MAX         80
+#define POWER_MIN         50
+#define NUM_LEDS          34
+#define PIN_DATA_LED      6
+int g_power;
+CRGB leds[NUM_LEDS];
+const int led = 13;
+/**
+ * @}
+*/
+
+/**
+ * @brief Gestion du watchdog
+ * @{
+*/
+bool g_flgWdg=true;
+long g_t0_ms=0;
+long g_delay_ms=3600L*1000L;
+/**
+ * @}
+*/
+
+#define SIZE_PROG (500)
+char prog[SIZE_PROG] = "S26E33LrOS1E23X200LgOWLrOWLbOWLgOWLrOWLbOWS26E33LgOS1E23X200LgOWLrOWLbOWLgOWLrOWLbOWS26E33LbOS1E23X200LgOWLrOWLbOWLgOWLrOWLbOW*";
+bool flg_runProg=false;
+LdAnim anim = LdAnim(leds, NUM_LEDS);
+
+/**
+ * @brief Allume toutes les LEDs avec la couleur fournie
+ * @param pLeds Buffer des LEDs
+ * @param a Couleur
+*/
+void setAll(CRGB *pLeds,CRGB a)
+{
+  for (int i=0;i<NUM_LEDS;i++)
+  {
+     pLeds[i] = a;
+  }
+}
+
+/**
+ * @brief Eteint toutes les LEDs
+ * @param pLeds Buffer des LEDs 
+*/
+void clearAll(CRGB *pLeds)
+{
+  setAll(pLeds,CRGB::Black);
+}
+
+/**
+ * @brief Controleur WS /leds/write
+ * 
+ * Ecrit toutes les LEDS
+ * La data dans le post est une suite d'octets codes en hexadecimal
+ * Trame: AA[Taille][R0][G0][B0]...[Rn][Gn][Bn]
+ * 
+ * Retourne "OK" si pris en compte
+ * 
+ * Exemple: AA06000000FF0000 => LED 0 eteinte et LED 1 en rouge
+*/
+void handleWrite()
+{  
+  if (g_flgWdg==true)
+    g_t0_ms=millis();
+  
+  if (server.hasArg("plain")== false)
+  {  
+    server.send(200, "text/plain", "Body not received");
+    return;
+  }
+
+  const char *pCmd=server.arg("plain").c_str();
+
+  int tmp=0;  
+  sscanf(pCmd,"%02x",&tmp);
+  if (tmp!=0xAA)
+  {  
+    server.send(200, "text/plain", "Bad magic");
+    return;
+  }
+
+  int len=0;
+  sscanf(pCmd+2,"%02x",&len);
+  Serial.print("Len:");
+  Serial.println(len);
+  
+  for (int i=0;i<len;i++)
+  {
+    if (i>NUM_LEDS)
+      break;
+      
+    int r,g,b;
+    char strVals[10];
+    memcpy(strVals,pCmd+4+(i*6),6);
+    strVals[6]=0;
+    Serial.println(strVals);
+
+    if (sscanf(strVals,"%02x%02x%02x",&r,&g,&b)==3)
+    {
+      Serial.print(i);
+      Serial.print(" ");
+      Serial.print("RGB: ");
+      Serial.print(r);
+      Serial.print(" ");
+      Serial.print(g);
+      Serial.print(" ");
+      Serial.println(b);
+
+      leds[i]=CRGB(r,g,b);
+    }
+  }
+  
+  FastLED.show();
+  
+  server.send(200, "text/plain", "OK");
+}
+
+/**
+ * @brief Controleur /leds/set
+ * 
+ * Parametres de la requete (dans l'URL):
+ *   - start: première LED de la selection
+ *   - end: derniere LED de la selection
+ *   - col: Couleur a appliquer 
+ *       - g: Vert
+ *       - b: Bleu
+ *       - w: Blanc
+ *       - r: Rouge
+ *       - 0: Etaint (zero)
+ *       - y: Jaune
+ *       - o: Orange
+ *       - p: Rose
+ *       - c: Cyan
+ *       - v: Violet
+ *   - r: Composante rouge
+ *   - g: composante verte
+ *   - b: composante bleue
+ *   
+ *   RETOURNE un JSON avec les valeurs appliquees en cas de reussite sinon ERROR
+ *   
+ *   Exemples: 
+ *     /leds/set?start=0&end=33&col=r  => les LEDs 0 a 33 en rouge
+ *     /leds/set?start=0&end=15&r=0&g=127&b=0  => les LEDs 0 a 15 en vert
+*/
+void handleSetLeds()
+{
+  if (g_flgWdg==true)
+    g_t0_ms=millis();
+
+  int start=-1,end=-1,r=-1,g=-1,b=-1;
+  
+  for (int i = 0; i < server.args(); i++) 
+  {
+    if (strcmp(server.argName(i).c_str(),"start")==0)
+    {
+      sscanf(server.arg(i).c_str(),"%d",&start);
+    }
+    else if (strcmp(server.argName(i).c_str(),"col")==0)
+    {
+      if (strcmp(server.arg(i).c_str(),"r")==0)
+      {
+        r=127;
+        g=0;
+        b=0;    
+      }
+      else if (strcmp(server.arg(i).c_str(),"g")==0)
+      {
+        r=0;
+        g=127;
+        b=0;    
+      }
+      else if (strcmp(server.arg(i).c_str(),"b")==0)
+      {
+        r=0;
+        g=0;
+        b=127;    
+      }
+      else if (strcmp(server.arg(i).c_str(),"w")==0)
+      {
+        r=127;
+        g=127;
+        b=127;    
+      }
+      else if (strcmp(server.arg(i).c_str(),"0")==0)
+      {
+        r=0;
+        g=0;
+        b=0;    
+      }
+      else if (strcmp(server.arg(i).c_str(),"y")==0)
+      {
+        r=127;
+        g=127;
+        b=0;    
+      }
+      else if (strcmp(server.arg(i).c_str(),"o")==0)
+      {
+        r=127;
+        g=82;
+        b=0;    
+      }
+      else if (strcmp(server.arg(i).c_str(),"p")==0)
+      {
+        r=127;
+        g=9;
+        b=73;    
+      }
+      else if (strcmp(server.arg(i).c_str(),"c")==0)
+      {
+        r=0;
+        g=127;
+        b=127;    
+      }
+      else if (strcmp(server.arg(i).c_str(),"v")==0)
+      {
+        r=127;
+        g=0;
+        b=127;    
+      }
+    }
+    else if (strcmp(server.argName(i).c_str(),"r")==0)
+    {
+      sscanf(server.arg(i).c_str(),"%d",&r);
+    }
+    else if (strcmp(server.argName(i).c_str(),"g")==0)
+    {
+      sscanf(server.arg(i).c_str(),"%d",&g);
+    }
+    else if (strcmp(server.argName(i).c_str(),"b")==0)
+    {
+      sscanf(server.arg(i).c_str(),"%d",&b);
+    }
+    else if (strcmp(server.argName(i).c_str(),"end")==0)
+    {
+      sscanf(server.arg(i).c_str(),"%d",&end);
+    }
+  }
+
+  if ( (start!=-1) && (r!=-1) && (g!=-1) && (b!=-1) )
+  {
+    if (start>=NUM_LEDS)
+      start=NUM_LEDS-1;
+    
+    if (end==-1)
+      end=start;
+  
+    if (end>=NUM_LEDS)
+      end=NUM_LEDS-1;
+
+    if (end<start)
+      end=start;
+
+    for (int i=start;i<=end;i++)
+    {
+      leds[i]=CRGB(r,g,b);
+    }
+
+    FastLED.show();
+
+    String s="{";
+    s+="start=";
+    s+=start;
+    s+=", end=";
+    s+=end;
+    s+=", rgb=\"(";
+    s+=r;
+    s+=",";
+    s+=g;
+    s+=",";
+    s+=b;
+    s+=")\"";
+    s+="}";
+    
+    server.send(200, "text/plain", s);
+    return;
+  }
+  
+  server.send(400, "text/plain", "ERROR");
+}
+
+/**
+ * @brief Controleur WS /leds/wdg/info
+ * 
+ * Retourne les informations du watchdog
+ * 
+ * {
+ *    "enabled":true,
+ *    "delay":456,
+ *    "elapsed":15
+ * }
+ * 
+*/
+void handleWdgInfo()
+{
+  long delta=(millis()-g_t0_ms)/1000;
+  
+  String s="{\"enabled\"=\"";
+  s+=g_flgWdg;
+  s+="\", \"delay\"=\"";
+  s+=(g_delay_ms/1000);
+  s+="\", \"elapsed\"=\"";
+  s+=delta;  
+  s+="\"}";
+  
+  server.send(200, "text/plain", s);
+}
+
+/**
+ * @brief Controleur WS /leds/wdg
+ * 
+ * Pilotage du watchdog
+ * 
+ *   - /leds/wdg => Rearme le watchdog. Retourne WDG OK.
+ *   - /leds/wdg?enable=1 => Active le watchdog. Retourne WDG ENABLED ou WDG DISABLED
+ *   - /leds/wdg?delay=456 => Configure le delai du watchdog (en s). Retourne WDG DELAY 456.
+*/
+void handleWdg()
+{
+  if (server.args()==0)
+  {
+    server.send(200, "text/plain", "WDG OK");
+    return;
+  }
+  else if (server.args()==1)
+  {
+    if (strcmp(server.argName(0).c_str(),"enable")==0)
+    {
+      if (strcmp(server.arg(0).c_str(),"1")==0)
+      {
+        g_flgWdg=true;
+        g_t0_ms=millis();
+        server.send(200, "text/plain", "WDG ENABLED");
+        return;
+      }
+      else
+      {
+        g_flgWdg=false;
+        server.send(200, "text/plain", "WDG DISABLED");
+        return;
+      }
+    }
+    else if (strcmp(server.argName(0).c_str(),"delay")==0)
+    {
+      long delay_s=0;
+      if (sscanf(server.arg(0).c_str(),"%ld",&delay_s)==1)
+      {
+        g_flgWdg=true;
+        g_delay_ms=delay_s*1000L;
+        g_t0_ms=millis();
+        String s="WDG DELAY ";
+        s+=delay_s;
+        s+=" seconds";
+        server.send(200, "text/plain", s);
+        return;
+      }
+    }
+  }
+
+  server.send(400, "text/plain", "ERROR");
+}
+
+/**
+ * @brief Controlleur WS eteint toutes les LEDs /leds/clearall
+*/
+void handleClearAll()
+{  
+  digitalWrite(led, 1);
+  clearAll(leds);
+  FastLED.show();
+    
+  server.send(200, "text/plain", "OK");
+  digitalWrite(led, 0);
+}
+
+/**
+ * @brief Controlleur WS informations
+ * 
+ * Retourne les informations sur le noeud
+*/
+void handleInfo()
+{    
+  digitalWrite(led, 1);
+  
+  String s="{";
+  s+="'name':'LedsTourMinou',";
+  s+="'description':'Gestion des LEDs de la tour du minou'";
+  s+="'num_leds':'";
+  s+=NUM_LEDS;
+  s+="'";
+  s+="}";
+  
+  server.send(200, "text/plain", s);
+  digitalWrite(led, 0);
+}
+
+/**
+ * @brief Controlleur WS pour gerer les programmes MACRO d'animation des LEDs
+ * 
+ * On envoi une chaine contenant le programme MACRO
+ * 
+ * - /leds/anim?prog="S26E33LrOS1E23..." => Modifie le programme en cours. Retourne un echo du programme si reussite.
+ * - /leds/anim?enable=1 => Active l'animation. Retourne ANIM ENABLED ou ANIM DISABLED
+ * - /leds/anim?info => Retourne la configuration de l'animation
+*/
+void handleAnim()
+{
+  if (server.args()==1)
+  {
+    if (strcmp(server.argName(0).c_str(),"prog")==0)
+    {
+      strncpy(prog,server.arg(0).c_str(),SIZE_PROG);
+      prog[SIZE_PROG-1]=0;
+      
+      server.send(200, "text/plain", prog);
+      return;
+    }
+    else if (strcmp(server.argName(0).c_str(),"enable")==0)
+    {
+      if (strcmp(server.arg(0).c_str(),"1")==0)
+      {
+        flg_runProg=true;
+        anim.init(prog, strlen(prog));
+        
+        server.send(200, "text/plain", "ANIM ENABLED");
+        return;
+      }
+      else
+      {
+        flg_runProg=false;
+        server.send(200, "text/plain", "ANIM DISABLED");
+        return;
+      }
+    }
+    else if (strcmp(server.argName(0).c_str(),"info")==0)
+    {
+      char str[100];
+      anim.getDumpStr(str,100);
+      server.send(200, "text/plain", str);   
+      return;  
+    }
+  }
+  server.send(400, "text/plain", "ERROR");     
+}
+
+/**
+ * @brief Controleur WS en cas d'erreur
+*/
+void handleNotFound()
+{
+  digitalWrite(led, 1);
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  for (uint8_t i = 0; i < server.args(); i++) {
+    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+  }
+  server.send(404, "text/plain", message);
+  digitalWrite(led, 0);
+}
+
+/**
+ * @brief Setup d'initialisation de l'arduino
+*/
+void setup(void)
+{
+  g_power=POWER_MAX;
+  pinMode(led, OUTPUT);
+  digitalWrite(led, 0);
+  Serial.begin(115200);
+
+  Wire.begin();
+
+  FastLED.addLeds<NEOPIXEL, PIN_DATA_LED>(leds, NUM_LEDS); 
+  FastLED.setBrightness(g_power);
+
+  setAll(leds,CRGB::Red);
+  FastLED.show();
+  delay(1000);
+  
+  clearAll(leds);
+  FastLED.show();
+  
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) 
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  if (MDNS.begin("esp8266")) 
+  {
+    Serial.println("MDNS responder started");
+  }
+
+  server.on("/leds/write", handleWrite);
+  server.on("/leds/clearall", handleClearAll);
+  server.on("/leds/info", handleInfo);
+  server.on("/leds/set", handleSetLeds);
+  server.on("/leds/wdg", handleWdg);
+  server.on("/leds/wdg/info", handleWdgInfo);  
+  server.on("/leds/anim", handleAnim); 
+  
+  server.onNotFound(handleNotFound);
+
+  server.begin();
+  Serial.println("HTTP server started");  
+
+  g_t0_ms=millis();
+}
+
+/**
+ * @loop Boucle principale de l'arduino
+*/
+void loop(void) 
+{
+  server.handleClient();
+  MDNS.update();
+
+  if (flg_runProg==true)
+  {
+    anim.tick();
+    delay(20);
+  }
+
+  if (g_flgWdg==true)
+  {
+    long t=millis();
+    long delta=0;
+    if (t>g_t0_ms)
+      delta=t-g_t0_ms;
+    else
+      delta=0xFFFFFFFF-g_t0_ms+t;
+    
+    if (delta>g_delay_ms)
+    {
+      g_t0_ms=millis();
+      clearAll(leds);
+      flg_runProg=false;
+      FastLED.show();
+    }    
+  }
+}
