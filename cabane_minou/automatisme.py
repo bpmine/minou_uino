f7c108eadce7af3bed983aa22b181723f5cd4ae8
bernard <bernard@pinochet.fr>
2022-12-05 23:53:00 +0100

    Animation Noël 2022
    - 6h à 6h30   : tour en bleu
    - 6h30 à 7h00 : tour et cabane haut en bleu
    - 7h à 7h15   : tour animée, cabane haut alterne vert/bleu ttes les mins et cabane bas en rouge
    - 7h15 à 7h30 : tour animée, cabane haut alterne rouge/vert ttes les mins et cabane bas en bleu
    - 7h30 à 7h45 : tour animée, cabane haut alterne rouge/bleu ttes les mins et cabane bas en vert
    - 7h45 à 8h   : tour animée, cabane haut animée et cabane bas en vert
    
    - 17h à 17h30 : tout vert
    - 17h30 à 18h : tour animée, cabane haut rouge, cabane bas vert
    - 18h à 19h   : tour animée, cabane haut animée, cabane bas bleu
    - 19h à 20h   : tour animée, cabane haut animée, cabane bas bleu
    - 20h à 21h   : tour animée, cabane haut animée, cabane bas rouge
    - 21h à 22h   : tour animée, cabane haut animée, cabane bas rouge
    - 22h à 23h   : tour animée, cabane haut rouge, cabane bas éteinte
    - 23h à 0h    : tour animée, cabane haut bleu, cabane bas éteinte
    - 0h à 6h     : tour alterne bleu, vert, rouge toutes les mins

--- cabane_minou/automatisme.py
+++ cabane_minou/automatisme.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 import os
-import datetime
+from datetime import datetime
 import time
 import requests
 
-IP_CABANE='192.168.1.44'
-IP_TOUR='192.168.1.42'
+IP_CABANE='192.168.2.155'
+IP_TOUR='192.168.2.130'
+
+LOG_PATH='/var/log/domgw'
+LOG_FILENAME='automatisme.log'
+LOG_ENABLED=False
 
 class ArdLeds:
     GREEN='g'
@@ -79,9 +83,7 @@ ledsCabaneBas=ArdLeds('cabane_bas',IP_CABANE,36,'leds/bas')
 ledsTour=ArdLeds('tour',IP_TOUR,33)
 
 
-#cmd_prog='S26E33LrOS1E23X800LgOWLrOWLbOWLgOWLrOWLbOWX800CS26E33LgOS1E23WX100LgOWLrOWLbOWLgOWLrOWLbOWLgOWLrOWLbOWLgOWLrOWLbOWX1000ZW*'
 PROG_INITIAL_TOUR='S26E33LrOS1E23X200LgOWLrOWLbOWLgOWLrOWLbOWS26E33LgOS1E23X200LgOWLrOWLbOWLgOWLrOWLbOWS26E33LbOS1E23X200LgOWLrOWLbOWLgOWLrOWLbOW*'
-
 PROG_1_CABANE='D2LrAWLgAWLbAW*'
 
 def clearAll():
@@ -94,113 +96,177 @@ def clearAll():
     ledsCabaneHaut.clrAll()
     ledsTour.clrAll()
 
+def log(msg):
+    dte=datetime.now()
+    sDte=dte.strftime('%d/%m/%Y %H:%M:%S')
+
+    print('%s> %s\n' % (sDte,msg))
+    
+    if LOG_ENABLED==True:        
+        with open(os.path.sep.join([LOG_PATH,LOG_FILENAME]),'w') as fp:                        
+            fp.write('%s> %s\n' % (sDte,msg))
+            fp.close()  
 
 
 def tick_minute(hour,minute):
-    print('Tick %d:%d' % (hour,minute) )
+    log('Tick %d:%d' % (hour,minute) )
+
+    try:
+        ledsCabaneBas.getWdg()
+    except Exception as e:
+        log('[ERREUR] - getWdg Cabane !')
     
+    try:
+        ledsTour.getWdg()
+    except Exception as e:
+        log('[ERREUR] - getWdg Cabane !')
+
+   
     if (hour==6) and (minute==0):
+        log('Debut animation du matin')
         ledsCabaneBas.disableAnim()
         ledsCabaneHaut.disableAnim()
-        
+        ledsTour.disableAnim()        
+        ledsCabaneBas.clrAll()
+        ledsCabaneHaut.clrAll()
+        ledsTour.setAll('b')
+    elif (hour==6) and (minute==30):
+        ledsCabaneBas.disableAnim()
+        ledsCabaneHaut.disableAnim()
+        ledsTour.disableAnim()        
         ledsCabaneBas.clrAll()
         ledsCabaneHaut.setAll('b')
-        ledsTour.setAnim(PROG_INITIAL_TOUR)
-
-    if (hour==7) and (minute==0):
-        ledsCabaneBas.disableAnim()
-        
-        ledsCabaneBas.clrAll()
-        ledsCabaneHaut.setAnim(PROG_1_CABANE)
-        ledsTour.setAnim(PROG_INITIAL_TOUR)
-
-    if (hour==9) and (minute==0):
+        ledsTour.setAll('b')
+    elif (hour==7) and (minute==0):
         ledsCabaneBas.disableAnim()
         ledsCabaneHaut.disableAnim()
-        ledsTour.disableAnim()
-        
+        ledsTour.disableAnim()        
+        ledsCabaneBas.setAll('r')
+        ledsCabaneHaut.setAll('g')
+        ledsTour.setAnim(PROG_INITIAL_TOUR)
+    elif (hour==7) and (minute>0) and (minute<15):
+        ledsCabaneBas.disableAnim()
+        ledsCabaneHaut.disableAnim()
+        ledsCabaneBas.setAll('r')
+        if (minute % 2) == 0:            
+            ledsCabaneHaut.setAll('b')
+        else:
+            ledsCabaneHaut.setAll('g')
+    elif (hour==7) and (minute>15) and (minute<30):
+        ledsCabaneBas.disableAnim()
+        ledsCabaneHaut.disableAnim()
+        ledsCabaneBas.setAll('b')        
+        if (minute % 2) == 0:
+            ledsCabaneHaut.setAll('b')
+        else:
+            ledsCabaneHaut.setAll('r')
+    elif (hour==7) and (minute>30) and (minute<45):
+        ledsCabaneBas.disableAnim()
+        ledsCabaneBas.clrAll()        
+        ledsCabaneHaut.disableAnim()
+        ledsCabaneBas.setAll('g')
+        if (minute % 2) == 0:
+            ledsCabaneHaut.setAll('b')
+        else:
+            ledsCabaneHaut.setAll('r')
+    elif (hour==7) and (minute==45):
+        ledsCabaneBas.disableAnim()
+        ledsCabaneHaut.setAnim(PROG_1_CABANE)
+        ledsTour.setAnim(PROG_INITIAL_TOUR)
+    elif (hour==8) and (minute==0):
+        log('Fin animation du matin')
+        ledsCabaneBas.disableAnim()
+        ledsCabaneHaut.disableAnim()
+        ledsTour.disableAnim()        
         ledsCabaneBas.clrAll()
         ledsCabaneHaut.clrAll()
         ledsTour.clrAll()
-
-    if (hour==17) and (minute==0):
-        ledsCabaneHaut.setWdg(3600*12)
-        ledsTour.setWdg(3600*12)
-        
+    elif (hour>8) and (hour<17) and (minute==0):
+        ledsCabaneBas.disableAnim()
+        ledsCabaneHaut.disableAnim()
+        ledsTour.disableAnim()        
+        ledsCabaneBas.clrAll()
+        ledsCabaneHaut.clrAll()
+        ledsTour.clrAll()
+    elif (hour==17) and (minute==0):
+        log('Debut animation du soir')        
         ledsCabaneBas.disableAnim()
         ledsCabaneHaut.disableAnim()
         ledsTour.disableAnim()
-
         ledsCabaneBas.setAll('g')
         ledsCabaneHaut.setAll('g')
-        ledsTour.setAll('g')
-        
-    if (hour==17) and (minute==30):
+        ledsTour.setAll('g')        
+    elif (hour==17) and (minute==30):
         ledsTour.disableAnim()
         ledsCabaneBas.setAll('g')
         ledsCabaneHaut.setAll('r')
         ledsTour.setAnim(PROG_INITIAL_TOUR)
-        
-    if (hour==18) and (minute==00):
+    elif (hour==18) and (minute==0):
         ledsCabaneBas.disableAnim()
         ledsCabaneBas.setAll('b')
         ledsCabaneHaut.setAnim(PROG_1_CABANE)
         ledsTour.setAnim(PROG_INITIAL_TOUR)
-
-    if (hour==22) and (minute==00):
+    elif (hour==19) and (minute==0):
+        ledsCabaneBas.disableAnim()
+        ledsCabaneBas.setAll('b')
+        ledsCabaneHaut.setAnim(PROG_1_CABANE)
+        ledsTour.setAnim(PROG_INITIAL_TOUR)
+    elif (hour==20) and (minute==0):
+        ledsCabaneBas.disableAnim()
+        ledsCabaneBas.setAll('b')
+        ledsCabaneHaut.setAnim(PROG_1_CABANE)
+        ledsTour.setAnim(PROG_INITIAL_TOUR)
+    elif (hour==21) and (minute==0):
+        ledsCabaneBas.disableAnim()
+        ledsCabaneBas.setAll('b')
+        ledsCabaneHaut.setAnim(PROG_1_CABANE)
+        ledsTour.setAnim(PROG_INITIAL_TOUR)
+    elif (hour==22) and (minute==0):
         ledsCabaneBas.disableAnim()
         ledsCabaneHaut.disableAnim()
-
         ledsCabaneBas.clrAll()        
         ledsCabaneHaut.setAll('r')
         ledsTour.setAnim(PROG_INITIAL_TOUR)
-
-    if (hour==23) and (minute==00):
+    elif (hour==23) and (minute==0):
         ledsCabaneBas.disableAnim()
         ledsCabaneHaut.disableAnim()
-
         ledsCabaneBas.clrAll()        
         ledsCabaneHaut.setAll('b')
         ledsTour.setAnim(PROG_INITIAL_TOUR)
-
-    if (hour==00) and (minute==00):
+    elif (hour==0) and (minute==0):
+        log('Fin anim du soir / Tour en veilleuse rouge puis bleu puis vert...')
         ledsCabaneBas.disableAnim()
         ledsCabaneHaut.disableAnim()
         ledsTour.disableAnim()
-
         ledsCabaneBas.clrAll()
         ledsCabaneHaut.clrAll()
         ledsTour.setAll('r')
-
-
-def run():
-    lh=None
-    lm=None
-    boot=True
-
-    while True:
-        now=datetime.datetime.now()
-        if boot==True:
-            print('BOOT')
-            lh=now.hour
-            lm=now.minute
-            boot=False
-            continue
-
-        h=now.hour
-        m=now.minute
-        if (h!=lh) or (m!=lm):
-            tick_minute(h,m)
-            
-        lh=now.hour
-        lm=now.minute
-
-        time.sleep(0.5)
-
-#run()
+    elif (hour>=0) and (hour<6):
+        ledsCabaneBas.disableAnim()
+        ledsCabaneHaut.disableAnim()
+        ledsTour.disableAnim()
+        ledsCabaneBas.clrAll()
+        ledsCabaneHaut.clrAll()
+        res=minute%3
+        if res==0:
+            ledsTour.setAll('r')
+        elif res==1:
+            ledsTour.setAll('g')
+        else:
+            ledsTour.setAll('b')
         
-now=datetime.datetime.now()
+
+if LOG_ENABLED==True:
+    if not os.path.exists(LOG_PATH):
+        os.mkdir(LOG_PATH)
+
+now=datetime.now()
 tick_minute(now.hour,now.minute)
+
+
+    
+        
+
 
 
     
